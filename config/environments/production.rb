1bf13d5fcce5e1fd0d61eafbb9307c6e404cd535
Jailan Salah <jailan.salah@gmail.com>
2015-03-22 08:31:58 +0200

    Use SSL and the Puma webserver in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,11 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
 
+  config.cache_classes = true
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
