c1a8c85df4efd26d53c649527245c4bef83e524b
jailan <jailan.salah@gmail.com>
2015-05-09 17:02:50 +0300

    trying assets drag

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+ 
 
 
 
