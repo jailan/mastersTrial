d9aede7dc051621fc8df2c5b0a945e290af3bcd2
jailan <jailan.salah@gmail.com>
2015-05-30 20:24:20 +0300

    trial

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -41,7 +41,10 @@ class GamesController < ApplicationController
       $speechy = $data['speech']
 
     end
-
+File.delete(Rails.root + 'app/assets/images/lalazy.mp3')
+File.open(Rails.root + 'app/assets/images/lalazy.mp3', "w+") do |f|
+  f.write("")
+end
 "#{$speechy}".to_file "ar", "app/assets/images/lalazy.mp3"
 
 end
